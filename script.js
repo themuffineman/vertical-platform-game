9c878170ac9261ec7da91b62f509f8fe61a1438f
Petrus <petrusheya@gmail.com>
2024-01-06 08:18:19 +0200

    Basic physics set up

---
+++ script.js
@@ -0,0 +1,127 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext("2d")
+
+canvas.width = 1024
+canvas.height = 576
+
+const gravity = 0.5
+const scaledCanvas = {
+    width: canvas.width/4,
+    height: canvas.height/4 
+}
+
+class Player {
+    constructor(position){
+        this.position = position
+        this.velocity = {
+            x:0,
+            y:1,
+        }
+        this.height = 100
+    }
+
+    draw(){
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x,this.position.y,100,this.height)
+    }
+    
+    update(){
+        this.draw()
+        this.position.y += this.velocity.y
+        this.position.x += this.velocity.x
+
+        if (this.position.y + this.velocity.y + this.height < canvas.height)
+        {this.velocity.y += gravity}
+
+        else {this.velocity.y = 0}
+    }
+}
+
+class Sprite{
+    constructor({position, imageSrc}) {
+
+        this.position = position
+        this.image = new Image()
+        this.image.src = imageSrc
+    }
+
+    draw(){
+        if (!this.image) return
+        c.drawImage(this.image, this.position.x, this.position.y)
+    }
+    update(){
+        this.draw()
+    }
+}
+
+const background = new Sprite({
+    position:{
+        x:0,
+        y:0,
+    },
+    imageSrc: './img/background.png'
+})
+
+const player = new Player({
+    x:0,
+    y:0,
+})
+const player2 = new Player({
+    x:300,
+    y:100,
+})
+const keys = {
+    d:{
+        pressed:false
+    },
+    a:{
+        pressed:false
+    }
+}
+function animate(){
+    window.requestAnimationFrame(animate)
+
+    c.fillStyle = 'white'
+
+    c.fillRect(0,0,canvas.width,canvas.height)
+
+    c.save()
+    c.scale(4,4)
+    c.translate(0, -background.image.height + scaledCanvas.height)
+    background.update()
+    c.restore()
+    player.update()
+    player2.update()
+
+    player.velocity.x = 0
+    if (keys.d.pressed) player.velocity.x = 5
+    else if (keys.a.pressed) player.velocity.x = -5
+    
+}
+
+animate()
+
+window.addEventListener('keydown', (event)=>{
+    switch (event.key){
+        case 'd':
+            keys.d.pressed =true
+            break
+        case 'a':
+            keys.a.pressed = true
+            break
+        case 'w':
+            player.velocity.y = -15
+            break
+    }
+})
+
+window.addEventListener('keyup', (event)=>{
+    switch (event.key){
+        case 'd':
+            keys.d.pressed =false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break
+    }
+})
